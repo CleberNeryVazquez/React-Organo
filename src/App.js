3d88c2e2fb1f7f838c2b019064c582343e34ed30
CleberNeryVazquez <123609475+CleberNeryVazquez@users.noreply.github.com>
2023-04-18 20:19:16 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Banner from "./componentes/Banner/Banner";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Banner />
+    </div>
+  );
+}
